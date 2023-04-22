6c10ff46d244c96e979841e8a7340a146fb8c7bc
Alexander Karasik <yakarasik@yandex.ru>
2023-04-22 23:58:52 +0300

    First assignment

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+const port =process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.use('/', require('./routes/index'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
