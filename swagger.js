a0718aa49168a6ae0a9af78aad98eca9e89e73ad
Alexander Karasik <yakarasik@yandex.ru>
2023-05-13 19:45:09 +0300

    changing host url for swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API for CSE-341',
     description: 'Contacts API, week 4; personal assignment',
   },
-  host: 'localhost:8080',
+  host: 'https://alexanderkarasik-cse341-node-week4.onrender.com/',
   schemes: ['http'],
 };
 
